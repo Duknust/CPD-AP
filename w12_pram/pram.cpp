33ca6e87791af327a40c6ca35dacffcf66e95f5a
Duarte Duarte <duartenuno1992@hotmail.com>
2015-05-18 16:51:50 +0100

    [Update] Fully functional and correct in place sum

--- w12_pram/pram.cpp
+++ w12_pram/pram.cpp
@@ -2,43 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <tgmath.h>
+#include <semaphore.h>
 #include <iostream>
 
 #define NUM_THREADS 4
 #define NUM 20
 
 int N=NUM;  // number of elements in array X
-int X[NUM];
-int gSum[NUM_THREADS];  // global storage for partial results
-pthread_mutex_t locks[NUM];
+int X[NUM];//={3,5,2,5,7,9,-4,6,7,-3,1,7,6,8,-1,2};
+int inTotals[NUM_THREADS];  // global storage for partial results
+int outTotals[NUM_THREADS];
+pthread_mutex_t locks[NUM_THREADS];
+sem_t semaforo[NUM_THREADS], semaforo2[NUM_THREADS];
+
+void printArray(int *array, int size){
+  for(int i=0; i<size; i++){
+    printf(" %d ",array[i]);
+  }
+  printf("\n ");
+}
 
 void *Summation (void *pArg)
 {
   int tNum = *((int *) pArg);
-  
-  std::cout << "tnum "<<tNum << "\n";
-  int lSum = 0;
-  int start, end;
-  int i,j=0,k;
+  int size = N/NUM_THREADS;
+  int start=tNum*size, end=(tNum+1)*size;
+  int j=0;
 
-  for(j=0;j<NUM-1;j++){
-    
 
-    pthread_mutex_lock(&(locks[tNum*2+j*NUM_THREADS]));
-    pthread_mutex_lock(&(locks[tNum*2+j*NUM_THREADS+1])); 
-    X[tNum*2+j*NUM_THREADS+1]+=X[tNum*2+j*NUM_THREADS];
-    //std::cout << tNum*2+j*NUM_THREADS << " "<< tNum*2+j*NUM_THREADS+1 << " ";
-    pthread_mutex_unlock(&(locks[tNum*2+j*NUM_THREADS]));
-    pthread_mutex_unlock(&(locks[tNum*2+j*NUM_THREADS+1]));
+  for(j=start+1;j<end;j++){
+    X[j]+=X[j-1];    
   }
 
-  //if(tNum==0){
-      std::cout <<"\n";
-      for(i=0;i<NUM;i++){
-        std::cout << X[i] << " ";
-      }
-    //}
+  inTotals[tNum]=X[end-1];
 
+  printf("numth_%d_start_%d_end_%d_mytotal_%d\n",tNum,start,end,inTotals[tNum]);
+
+  sem_post(&semaforo2[tNum]);
+  sem_wait(&semaforo[tNum]);
+
+  for (j=start; j<end; j++){
+    X[j]+=outTotals[tNum];
+  }
+
+  return 0;
 }
 
 void InitializeArray(int *S, int *N)
@@ -51,21 +58,56 @@ void InitializeArray(int *S, int *N)
 
 int main(int argc, char* argv[])
 {
-  int j, sum = 0;
+  int j;
   pthread_t tHandles[NUM_THREADS];
 
   InitializeArray(X,&N);  // get values into A array; not shown
-  for(j=0; j<NUM ;j++)
-    pthread_mutex_init(&locks[j], NULL);
+  for(j=0;j<NUM_THREADS;j++){
+    sem_init(&semaforo[j], 0, 0);
+    sem_init(&semaforo2[j], 0, 0);
+  }
 
   for (j = 0; j < NUM_THREADS; j++) {
     int *threadNum = (int*)malloc(sizeof (int));
     *threadNum = j;
     pthread_create(&tHandles[j], NULL, Summation, (void *)threadNum);
   }
+
+  printf("x\n");
+  printArray(X,NUM);
+  printf("\n");
+
+
+  for(j=0;j<NUM_THREADS;j++){
+    sem_wait(&semaforo2[j]);
+  }
+
+  printf("inTotals\n");
+  printArray(inTotals,NUM_THREADS);
+  printf("\n");  
+
+  outTotals[0]=0;
+  for(j=1; j<NUM_THREADS;j++){
+    outTotals[j]=inTotals[j-1]+outTotals[j-1];
+  }
+
+  printf("outTotals\n");
+  printArray(outTotals,NUM_THREADS);
+  printf("\n");  
+
+  for (j=0;j<NUM_THREADS;j++){ //threads podem voltar a trabalhar
+    sem_post(&semaforo[j]); 
+  }
+
   for (j = 0; j < NUM_THREADS; j++) {
     pthread_join(tHandles[j], NULL);
   }
   printf("The sum of array elements is %d\n", X[N-1]);
+  for(j=0;j<NUM_THREADS;j++){
+    sem_destroy(&semaforo[j]); 
+    sem_destroy(&semaforo2[j]);
+  }
   return 0;
 }
+
+
